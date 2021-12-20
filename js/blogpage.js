793066f03ef2b149ba289c48f2a63d55c0369ad6
Nikhil Chitte <chnikhil250@gmail.com>
2021-12-20 17:25:22 +0530

    'initial update for test'

---
+++ js/blogpage.js
@@ -0,0 +1,39 @@
+const api_key = 'AIzaSyDA2EpnBDQcNehCydte_UqPLhZO0bGC9lk'
+const apilink = '?key='+api_key
+const url = 'https://www.googleapis.com/blogger/v3/blogs/6246307111365338254/posts?key=AIzaSyDA2EpnBDQcNehCydte_UqPLhZO0bGC9lk';
+fetch(url)
+.then( (res) => {return res.json()})
+.then((result) => {
+    var arr = result.items;
+    n = arr.length;
+	var i = 0;
+    for(i ; i < n; i++){
+        var data = `
+        <div class="blog-post">
+						<center>
+                        <h1 class="title"><a target="_blank" href="${arr[i].url}">${arr[i].title}</a></h1>
+						<ul class="post-meta">
+							<li>
+								<a href="#">${arr[i].published.substr(0,10)}</a>
+							</li>
+							<li>
+								<a href="#">Honey</a>
+							</li>
+							<li>
+								<a id="comments-anchor" target="_blank" href="${arr[i].url+'#comments'}">${arr[i].replies.totalItems} comments</a>
+							</li>
+						</ul>
+                        </center>
+						<p>${arr[i].content}</p> 
+						<ul class="share-post">
+							<li><a class="like"><i class="fa fa-heart"></i></a></li>
+							<li><a class="comment" href="${arr[i].url+'#comments'}" target="_blank"><i class="fa fa-comment"></i></a></li>	
+							<li><a class="share" id="${'share '+i}" data-clipboard-text="${arr[i].url}"><i class="fa fa-share"><input type="button" style="display : none; cursor : pointer;" id="copy" data-clipboard-target="#txt_copy" >
+							</i></a></li>
+						</ul>
+		</div>
+        `
+        document.getElementById('root').innerHTML += data;
+    }
+})
+.catch(err => console.warn(err))
---
+++ js/ordernow.js
@@ -0,0 +1,91 @@
+$(document).ready(function() {
+    $('input[type="checkbox"]').click(function() {
+        var inputValue = $(this).attr("value");  
+        $("." + inputValue).toggle();
+    });
+});
+
+function plus(){
+    const thing = document.getElementById('carom').parentElement;
+    const m = thing.children[2]
+    m.value ++;
+} 
+function minus(){
+    const thing = document.getElementById('carom').parentElement;
+    const m = thing.children[2];
+    if (m.value > 0){
+        m.value --;
+    }
+}
+function plus1(){
+    const thing = document.getElementById('jamun').parentElement;
+    const m = thing.children[2]
+    m.value ++;
+}
+function minus1(){
+    const thing = document.getElementById('jamun').parentElement;
+    const m = thing.children[2];
+    if (m.value > 0){
+        m.value --;
+    }
+}
+function plus2(){
+    const thing = document.getElementById('litchi').parentElement;
+    const m = thing.children[2]
+    m.value ++;
+}
+function minus2(){
+    const thing = document.getElementById('litchi').parentElement;
+    const m = thing.children[2];
+    if (m.value > 0){
+        m.value --;
+    }
+}
+function plus4(){
+    const thing = document.getElementById('mustard').parentElement;
+    const m = thing.children[2]
+    m.value ++;
+}
+function minus4(){
+    const thing = document.getElementById('mustard').parentElement;
+    const m = thing.children[2];
+    if (m.value > 0){
+        m.value --;
+    }
+}
+function plus5(){
+    const thing = document.getElementById('coriander').parentElement;
+    const m = thing.children[2]
+    m.value ++;
+}
+function minus5(){
+    const thing = document.getElementById('coriander').parentElement;
+    const m = thing.children[2];
+    if (m.value > 0){
+        m.value --;
+    }
+}
+function plus6(){
+    const thing = document.getElementById('eucalyptus').parentElement;
+    const m = thing.children[2]
+    m.value ++;
+}
+function minus6(){
+    const thing = document.getElementById('eucalyptus').parentElement;
+    const m = thing.children[2];
+    if (m.value > 0){
+        m.value --;
+    }
+}
+function plus3(){
+    const thing = document.getElementById('tulsi').parentElement;
+    const m = thing.children[2]
+    m.value ++;
+}
+function minus3(){
+    const thing = document.getElementById('tulsi').parentElement;
+    const m = thing.children[2];
+    if (m.value > 0){
+        m.value --;
+    }
+}
