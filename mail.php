f7378a0a84360b6c1be667b6d0f57f97e046d74e
Nikhil Chitte <chnikhil250@gmail.com>
2021-12-23 17:21:23 +0530

    form

--- mail.php
+++ mail.php
@@ -4,10 +4,10 @@ $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
 $to = "chittenikhil250@gmail.com";
-$subject = "Message From Website";
+$subject = "Message From thegreathoneyco.com";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
 $headers = "From: noreply@tgh.in" . "\r\n" .
-"CC: somebodyelse@example.com";
+"CC: chnikhil250@gmail.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
