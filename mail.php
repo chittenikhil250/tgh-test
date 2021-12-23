8858b47edb31a7eb1eef05f308421286bff14797
Nikhil Chitte <chnikhil250@gmail.com>
2021-12-23 17:41:47 +0530

    contact form 5

--- mail.php
+++ mail.php
@@ -6,9 +6,9 @@ $message= $_POST['message'];
 $to = "chittenikhil250@gmail.com";
 $subject = "Message From thegreathoneyco.com";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@tgh.in" . "\r\n" .
+$headers = "From: noreply@tghc.in" . "\r\n" .
 "CC: chnikhil250@gmail.com";
-if($email!=NULL){
+if(isset($_POST['submit'])){ 
     mail($to,$subject,$txt,$headers);
 }
 header("Location:ordernow.html");
